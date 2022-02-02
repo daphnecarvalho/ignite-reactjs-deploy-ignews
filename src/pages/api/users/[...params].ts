0342364058ee196ab6c10c764d511f028ee7178a
Daphne Carvalho <dah.feh@hotmail.com>
2022-02-02 00:00:59 -0300

    revert: delete unused files

--- src/pages/api/users/[...params].ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function Users(request: NextApiRequest, response: NextApiResponse) {
-  const { params } = request.query;
-
-  return response.json(params);
-}
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,13 +1,12 @@
 import Stripe from 'stripe';
-import { version } from '../../package.json';
 
 export const stripe = new Stripe(
   process.env.STRIPE_API_SECRET_KEY,
   {
     apiVersion: '2020-08-27',
     appInfo: {
-      name: 'igmews',
-      version
+      name: "igmews",
+      version: "0.1.0"
     }
   }
 );
